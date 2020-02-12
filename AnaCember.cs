158fc5122bc0ff60e7c5df2f2ec728fd7a39f3db
utakgames <60888280+utakgames@users.noreply.github.com>
2020-02-12 15:00:26 +0300

    Add files via upload

---
+++ AnaCember.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaCember : MonoBehaviour
+{
+    public GameObject KucukCember;
+    GameObject OyunYoneticisi;
+    void Start()
+    {
+        OyunYoneticisi = GameObject.FindGameObjectWithTag("oyunyoneticisitag");
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            kucukCemberOlustur();
+        }
+    }
+    void kucukCemberOlustur()
+    {
+        Instantiate(KucukCember, transform.position, transform.rotation);
+        OyunYoneticisi.GetComponent<oyunYoneticisi>().KucukCemberlerdeTextGosterme;
+    }
+}
