afe3bb597ddde2243d72704a2205bb396b9daf18
utakgames <60888280+utakgames@users.noreply.github.com>
2020-02-12 15:02:13 +0300

    Add files via upload

---
+++ KucukCemberKod.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KucukCemberKod : MonoBehaviour
+{
+    Rigidbody2D fizik;
+    public float hiz;
+    bool HareketKontrol = false;
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {   if(!HareketKontrol)
+        {
+            fizik.MovePosition(fizik.position + Vector2.up * hiz * Time.deltaTime); 
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag=="donencember")
+        {
+            transform.SetParent(col.transform);
+            HareketKontrol = true;
+        }
+    }
+}
---
+++ oyunYoneticisi.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class oyunYoneticisi : MonoBehaviour
+{
+    public Text DonenCemberLevel;
+    GameObject donenCember;
+    GameObject anaCember;
+    public Animator animator;
+    public Text bir;
+    public Text iki;
+    public Text uc;
+    public int kacTaneKucukCemberOlsun;
+    bool kontrol = true;
+    void Start()
+    {
+        PlayerPrefs.SetInt("kayit", int.Parse(SceneManager.GetActiveScene().name));
+
+        donenCember = GameObject.FindGameObjectWithTag("donencembertag");
+        anaCember = GameObject.FindGameObjectWithTag("anacembertag");
+
+        DonenCemberLevel.text = SceneManager.GetActiveScene().name;
+
+        if (kacTaneKucukCemberOlsun < 3)
+        {
+            bir.text = kacTaneKucukCemberOlsun + "";
+            iki.text = (kacTaneKucukCemberOlsun - 1) + "";
+        }
+        else if (kacTaneKucukCemberOlsun < 2)
+        {
+            bir.text = kacTaneKucukCemberOlsun + "";
+        }
+        else
+            bir.text = kacTaneKucukCemberOlsun + "";
+            iki.text = (kacTaneKucukCemberOlsun - 1) + "";
+            uc.text = (kacTaneKucukCemberOlsun - 2) + "";
+    }
+    public void KucukCemberlerdeTextGosterme()
+    {
+        if (kacTaneKucukCemberOlsun < 3)
+        {
+            bir.text = kacTaneKucukCemberOlsun + "";
+            iki.text = (kacTaneKucukCemberOlsun - 1) + "";
+            uc.text = "";
+        }
+        else if (kacTaneKucukCemberOlsun < 2)
+        {
+            bir.text = kacTaneKucukCemberOlsun + "";
+            iki.text = "";
+            uc.text = "";
+        }
+        else
+            bir.text = kacTaneKucukCemberOlsun + "";
+        iki.text = (kacTaneKucukCemberOlsun - 1) + "";
+        uc.text = (kacTaneKucukCemberOlsun - 2) + "";
+
+        if(kacTaneKucukCemberOlsun==0)
+        {
+            StartCoroutine(yeniLevel());
+        }
+    }
+    IEnumerator yeniLevel()
+    {
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        anaCember.GetComponent<AnaCember>().enabled = false;
+        yield return new WaitForSeconds(2);
+        if(kontrol)
+        {
+            animator.SetTrigger("yenilevel");
+            yield return new WaitForSeconds(3);
+            SceneManager.LoadScene(int.Parse(SceneManager.GetActiveScene().name) + 1);
+
+        }
+
+    }
+
+    public void OyunBitti()
+    {
+        StartCoroutine(CagrilanMetot());
+    }
+    IEnumerator CagrilanMetot()
+    {
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        anaCember.GetComponent<AnaCember>().enabled = false;
+
+        animator.SetTrigger("oyunbitti");
+        kontrol = false;
+
+        yield return new WaitForSeconds(3);
+      
+        SceneManager.LoadScene("anaMenu");
+    }
+}
